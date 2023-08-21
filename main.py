579e2afb83c534119f7fbf0a434fea54a8a733d4
Piche Traful <juliocaesar1@gmail.com>
2023-08-21 15:48:33 +0200

    Game to guess all 50 states from US
    files supporting the state guessing game

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
-import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+import turtle
+import pandas
 
-'''
-Create a turtle player that starts at the bottom of the screen and listen for the "Up" keypress to move the turtle north. 
-'''
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-player = Player()
-car_manager = CarManager()
-scoreboard = Scoreboard()
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
 
-screen.listen()
-screen.onkey(player.go_up, "Up")
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+turtle.shape(image)
 
-    car_manager.create_car()
-    car_manager.move_cars()
 
-    # Detect collision with cars
-    for car in car_manager.all_cars:
-        if car.distance(player) < 20:
-            game_is_on = False
-            scoreboard.game_over()
 
-    # Detect successful crossing
-    if player.is_at_finish_line():
-        player.go_to_start()
-        car_manager.level_up()
-        scoreboard.increase_level()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-screen.exitonclick()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
+                                    prompt="What's another state's name?:").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
