08774829efc37be958f2b4a1a66a3aca0b49ffd9
Piche Traful <juliocaesar1@gmail.com>
2023-08-29 13:35:43 +0200

    TKINTER gui Miles to KM convertion
    GUI creation using tkinter to calculate Miles to KM

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
+from tkinter import *
 
-import pandas
-# student_data_frame = pandas.DataFrame(student_dict)
-nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-#Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
+def button_clicked():
+    print("I got clicked")
+    new_text = round(float(input.get()) * 1.609, 2)
+    my_label2.config(text=new_text)
 
-# Keyword Method with iterrows()
-nato_alphabet_dict = {row["letter"]:row["code"] for (index, row) in nato_alphabet.iterrows()}
-# print(nato_alphabet_dict)
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
+window = Tk()
+window.title("Miles to KM convertor")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# labels
+my_label = Label(text="Is equal to", font=("Arial", 24, "bold"))
+# my_label["text"] = "New Text"
+#my_label.config(text="New Text")
+# my_label.pack()
+my_label.grid(column=0, row=1)
 
-name_to_divide = input("Enter a word: ").upper()
+my_label2 = Label(text="0", font=("Arial", 24, "bold"))
+my_label2.grid(column=1, row=1)
 
-name_to_alphabet = [nato_alphabet_dict[letter] for letter in name_to_divide]
+my_label3 = Label(text="Km", font=("Arial", 24, "bold"))
+my_label3.grid(column=2, row=1)
 
-print(name_to_alphabet)
+my_label4 = Label(text="Miles", font=("Arial", 24, "bold"))
+my_label4.grid(column=2, row=0)
+
+# Button
+button = Button(text="Click me", command=button_clicked)
+# button.pack()
+button.grid(column=1, row=2)
+
+# Entry
+input = Entry(width=10)
+print(input.get())
+# input.pack()
+input.grid(column=1, row=0)
+
+
+
+window.mainloop()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
