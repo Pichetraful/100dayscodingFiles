39325546ec0549bc2bf6a41e6409e44766abcc08
Piche Traful <juliocaesar1@gmail.com>
2023-09-07 10:12:33 +0200

    SMTP python code to send email with quotes
    code to send email to people with quotes. In order to work you need to get your password and also if desired change to your email.

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-from tkinter import *
+import smtplib
+#
+my_email = "juliocaesar1@gmail.com"
+password = "HERETHEPWUGOTFROMGOOGLEMAIL"
 
-def button_clicked():
-    print("I got clicked")
-    new_text = round(float(input.get()) * 1.609, 2)
-    my_label2.config(text=new_text)
-
-window = Tk()
-window.title("Miles to KM convertor")
-window.minsize(width=500, height=300)
-window.config(padx=20, pady=20)
-
-# labels
-my_label = Label(text="Is equal to", font=("Arial", 24, "bold"))
-# my_label["text"] = "New Text"
-#my_label.config(text="New Text")
-# my_label.pack()
-my_label.grid(column=0, row=1)
-
-my_label2 = Label(text="0", font=("Arial", 24, "bold"))
-my_label2.grid(column=1, row=1)
-
-my_label3 = Label(text="Km", font=("Arial", 24, "bold"))
-my_label3.grid(column=2, row=1)
-
-my_label4 = Label(text="Miles", font=("Arial", 24, "bold"))
-my_label4.grid(column=2, row=0)
-
-# Button
-button = Button(text="Click me", command=button_clicked)
-# button.pack()
-button.grid(column=1, row=2)
-
-# Entry
-input = Entry(width=10)
-print(input.get())
-# input.pack()
-input.grid(column=1, row=0)
+import datetime as dt
+import random
 
 
+now = dt.datetime.now()
+#year = now.year
+# if year == 2020:
+#     print("wear a face mask")
+# else:
+#     print("we don't use face masks anymore")
+# print(year)
+# month = now.month
+day_of_week = now.weekday()
+if day_of_week == 2:
+    with open("quotes.txt", "r") as quotes:
+        list_quotes = quotes.read().splitlines()
+        today_q = random.choice(list_quotes)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="juliocaeasar@hotmail.com", msg=f"Subject: Quote of the Day\n\n{today_q}")
 
-window.mainloop()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# date_of_birth = dt.datetime(year=1968, month=11, day=11, hour=2)
+# print(date_of_birth)
